d17e3142efb484a44287ecac3e7492334fe26b1d
hydreliox <hydreliox@gmail.com>
2016-09-13 18:38:39 +0200

    Change structure to be able to submit the package to Python Package Index

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'pybbox',
+  packages = ['pybbox'], # this must be the same as the name above
+  version = '0.0.1-alpha',
+  description = 'a simple python3 library for the Bouygues BBox Routeur API',
+  author = 'Hydreliox',
+  author_email = 'hydreliox@gmail.com',
+  url = 'https://github.com/HydrelioxGitHub/pybbox', # use the URL to the github repo
+  download_url = 'https://github.com/HydrelioxGitHub/pybbox/tarball/0.0.1-alpha', # I'll explain this in a second
+  keywords = ['bbox', 'Bouygues', 'routeur', 'API'], # arbitrary keywords
+  classifiers = [],
+)
