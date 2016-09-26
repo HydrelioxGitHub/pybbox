61a5e36c64f68ac8bb8785fd90a2a4ef30c88430
hydreliox <hydreliox@gmail.com>
2016-09-26 22:41:28 +0200

    Changes in API call structure (use only one  BboxAuth and BboxApiUrl instance) Add login and logout Add a method to set the luminosity level on the box

--- pybbox/bboxApiURL.py
+++ pybbox/bboxApiURL.py
@@ -20,7 +20,9 @@ class BboxAPIUrl:
         self.api_class = api_class
         self.api_method = api_method
         self.ip = ip
-        self.url = self.build_url_request()
+        self.authentication_type = None
+        self.url = None
+        self.build_url_request()
 
     def get_api_class(self):
         return self.api_class
@@ -34,6 +36,14 @@ class BboxAPIUrl:
     def get_url(self):
         return self.url
 
+    def get_authentication_type(self):
+        return self.authentication_type
+
+    def set_api_name(self, api_class, api_method):
+        self.api_class = api_class
+        self.api_method = api_method
+        self.build_url_request()
+
     def build_url_request(self):
         """
         Build the url to use for making a call to the Bbox API
@@ -42,9 +52,11 @@ class BboxAPIUrl:
         # Check if the ip is LAN or WAN
         if net.IPAddress(self.ip).is_private():
             url = "http://{}".format(self.ip)
+            self.authentication_type = BboxConstant.AUTHENTICATION_TYPE_LOCAL
         else:
             url = "https://{}:{}".format(self.ip,
                                          BboxConstant.DEFAULT_REMOTE_PORT)
+            self.authentication_type = BboxConstant.AUTHENTICATION_TYPE_REMOTE
 
         if self.api_class is None:
             url = "{}/{}".format(url, self.API_PREFIX)
@@ -52,6 +64,6 @@ class BboxAPIUrl:
             url = "{}/{}/{}".format(url, self.API_PREFIX, self.api_class)
 
         if self.api_method is None:
-            return url
+            self.url = url
         else:
-            return "{}/{}".format(url, self.api_method)
+            self.url = "{}/{}".format(url, self.api_method)
--- pybbox/pybbox.py
+++ pybbox/pybbox.py
@@ -1,6 +1,4 @@
 import netaddr as net
-import logging
-import requests
 from boxApiCall import BoxApiCall
 from bboxConstant import BboxConstant
 from bboxAuth import BboxAuth
@@ -18,12 +16,8 @@ class Bbox:
         Initiate a Bbox instance with a default local ip (192.168.1.254)
         :param ip: ip of the bow
         """
-        self.ip = ip
-        if net.IPAddress(self.ip).is_private():
-            self.authentication_type = BboxConstant.AUTHENTICATION_TYPE_LOCAL
-        else:
-            self.authentication_type = BboxConstant.AUTHENTICATION_TYPE_REMOTE
-        self.authenticated = False
+        self.bbox_url = BboxAPIUrl(None, None, ip)
+        self.bbox_auth = BboxAuth(None, None, False, self.bbox_url.authentication_type)
 
     @property
     def get_access_type(self):
@@ -31,7 +25,7 @@ class Bbox:
         Return if the access is made on the local network or remotely
         :return: AUTHENTICATION_TYPE_LOCAL or AUTHENTICATION_TYPE_REMOTE
         """
-        return self.authentication_type
+        return self.bbox_url.authentication_type
 
     """
     USEFUL FUNCTIONS
@@ -45,10 +39,28 @@ class Bbox:
         """
 
         """
-        auth = BboxAuth(BboxConstant.AUTHENTICATION_LEVEL_PUBLIC, BboxConstant.AUTHENTICATION_LEVEL_PRIVATE, self.authenticated, self.authentication_type)
-        api_url = BboxAPIUrl(BboxConstant.API_DEVICE, None, self.ip)
-        api = BoxApiCall(api_url, BboxConstant.HTTP_METHOD_GET, None, auth)
-        return api.execute_api_request()
+        self.bbox_auth.set_access(BboxConstant.AUTHENTICATION_LEVEL_PUBLIC, BboxConstant.AUTHENTICATION_LEVEL_PRIVATE)
+        self.bbox_url.set_api_name(BboxConstant.API_DEVICE, None)
+        api = BoxApiCall(self.bbox_url, BboxConstant.HTTP_METHOD_GET, None,
+                         self.bbox_auth)
+        resp = api.execute_api_request()
+        return resp.json()[0]
+
+    def set_display_luminosity(self, luminosity):
+        """
+        :param luminosity: int must be between 0 (light off) and 100
+        """
+
+        if (luminosity < 0) or (luminosity > 100):
+            raise ValueError("Luminosity must be between 0 and 100")
+        self.bbox_auth.set_access(BboxConstant.AUTHENTICATION_LEVEL_PRIVATE,
+                                  BboxConstant.AUTHENTICATION_LEVEL_PRIVATE)
+        self.bbox_url.set_api_name(BboxConstant.API_DEVICE, "display")
+        data = {'luminosity': luminosity}
+        api = BoxApiCall(self.bbox_url, BboxConstant.HTTP_METHOD_PUT, data,
+                         self.bbox_auth)
+        response = api.execute_api_request()
+        return response
 
     """
     LAN API
@@ -59,10 +71,12 @@ class Bbox:
         Get all info about devices connected to the box
         :return: a list with all devices data
         """
-        auth = BboxAuth(BboxConstant.AUTHENTICATION_LEVEL_PUBLIC, BboxConstant.AUTHENTICATION_LEVEL_PRIVATE, self.authenticated, self.authentication_type)
-        api_url = BboxAPIUrl(BboxConstant.API_HOSTS, None, self.ip)
-        api = BoxApiCall(api_url, BboxConstant.HTTP_METHOD_GET, None, auth)
-        return api.execute_api_request()["hosts"]["list"]
+        self.bbox_auth.set_access(BboxConstant.AUTHENTICATION_LEVEL_PUBLIC, BboxConstant.AUTHENTICATION_LEVEL_PRIVATE)
+        self.bbox_url.set_api_name(BboxConstant.API_HOSTS, None)
+        api = BoxApiCall(self.bbox_url, BboxConstant.HTTP_METHOD_GET, None,
+                         self.bbox_auth)
+        resp = api.execute_api_request()
+        return resp.json()[0]["hosts"]["list"]
 
     def is_device_connected(self, ip):
         """
@@ -77,6 +91,39 @@ class Bbox:
         return False
 
     """
+    USER ACCOUNT
+    """
+
+    def login(self, password):
+        """
+
+        :param password:
+        :return:
+        """
+        self.bbox_auth.set_access(BboxConstant.AUTHENTICATION_LEVEL_PUBLIC, BboxConstant.AUTHENTICATION_LEVEL_PUBLIC)
+        self.bbox_url.set_api_name("login", None)
+        data = {'password': password}
+        api = BoxApiCall(self.bbox_url, BboxConstant.HTTP_METHOD_POST, data,
+                         self.bbox_auth)
+        response = api.execute_api_request()
+        if response.status_code == 200:
+            self.bbox_auth.set_cookie_id(response.cookies["BBOX_ID"])
+        return self.bbox_auth.is_authentified()
+
+    def logout(self):
+        """
+        :return:
+        """
+        self.bbox_auth.set_access(BboxConstant.AUTHENTICATION_LEVEL_PUBLIC, BboxConstant.AUTHENTICATION_LEVEL_PUBLIC)
+        self.bbox_url.set_api_name("logout", None)
+        api = BoxApiCall(self.bbox_url, BboxConstant.HTTP_METHOD_POST, None,
+                         self.bbox_auth)
+        response = api.execute_api_request()
+        if response.status_code == 200:
+            self.bbox_auth.set_cookie_id(None)
+        return not self.bbox_auth.is_authentified()
+
+    """
     WAN API
     """
 
@@ -85,10 +132,12 @@ class Bbox:
         Get all info about the xdsl connection
         :return: a list with all devices data
         """
-        auth = BboxAuth(BboxConstant.AUTHENTICATION_LEVEL_PUBLIC, BboxConstant.AUTHENTICATION_LEVEL_PRIVATE, self.authenticated, self.authentication_type)
-        api_url = BboxAPIUrl(BboxConstant.API_WAN, "xdsl", self.ip)
-        api = BoxApiCall(api_url, BboxConstant.HTTP_METHOD_GET, None, auth)
-        return api.execute_api_request()["wan"]["xdsl"]
+        self.bbox_auth.set_access(BboxConstant.AUTHENTICATION_LEVEL_PUBLIC, BboxConstant.AUTHENTICATION_LEVEL_PRIVATE)
+        self.bbox_url.set_api_name(BboxConstant.API_WAN, "xdsl")
+        api = BoxApiCall(self.bbox_url, BboxConstant.HTTP_METHOD_GET, None,
+                         self.bbox_auth)
+        resp = api.execute_api_request()
+        return resp.json()[0]["wan"]["xdsl"]
 
     def is_bbox_connected(self):
         """
